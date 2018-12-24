f7955b69a5beaa493125da24ffc82438d00a29a0
coindev9 <46055424+coindev9@users.noreply.github.com>
2018-12-24 06:46:00 -0600

    Add files via upload

---
+++ decode_58.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python 
+"""
+This script convert private key into wallet format
+usage:
+python private_to_wallet_format.py private_key
+"""
+
+import sys
+import hashlib
+
+
+__b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+__b58base = len(__b58chars)
+b58chars = __b58chars
+
+def b58decode(v, length = None):
+    """ decode v into a string of len bytes
+    """
+    long_value = 0
+    for i, c in enumerate(v[::-1]):
+        pos = __b58chars.find(c)
+        assert pos != -1
+        long_value += pos * (__b58base**i)
+
+    result = bytes()
+    while long_value >= 256:
+        div, mod = divmod(long_value, 256)
+        result = chr(mod) + result
+        long_value = div
+    result = chr(long_value) + result
+
+    nPad = 0
+    for c in v:
+        if c == __b58chars[0]:
+            nPad += 1
+            continue
+        break
+
+    result = bytes(nPad) + result
+    if length is not None and len(result) != length:
+        return None
+
+    return result
+
+def main():
+
+
+    address = sys.argv[1]
+    res = b58decode(address)
+    decoded = str(res.encode('hex'))
+    print decoded
+    print decoded[4:-8]
+
+if __name__ == '__main__':
+    main()
